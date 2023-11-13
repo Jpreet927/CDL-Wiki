66f1e8e88d77b4f2e322369a6f7a7e1d9ac59faa
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-11-13 17:28:02 -0500

    feat: :tada: initial commit

---
+++ backend/src/main/java/com/jpreet/cdlwiki/CdlWikiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jpreet.cdlwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdlWikiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdlWikiBackendApplication.class, args);
+	}
+
+}
