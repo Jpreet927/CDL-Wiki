6dd8ce50403b2331d9a34edf62b028cea14341f8
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-11-19 17:08:02 -0500

    fix: :bug: added getters and setters

--- backend/src/main/java/com/jpreet/cdlwiki/model/Player.java
+++ backend/src/main/java/com/jpreet/cdlwiki/model/Player.java
@@ -19,4 +19,68 @@ public class Player {
     private String nationality;
     private String role;
     private String image;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
