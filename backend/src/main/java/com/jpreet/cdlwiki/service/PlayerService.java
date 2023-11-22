60c8426853d7d3f56b133b25f959219eca95ae0b
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-11-22 18:21:22 -0500

    feat: :sparkles: defined methods for fetching all players/teams and by id

--- backend/src/main/java/com/jpreet/cdlwiki/service/PlayerService.java
+++ backend/src/main/java/com/jpreet/cdlwiki/service/PlayerService.java
@@ -1,12 +1,44 @@
 package com.jpreet.cdlwiki.service;
 
+import com.jpreet.cdlwiki.dto.PlayerDTO;
+import com.jpreet.cdlwiki.exception.CDLWikiException;
+import com.jpreet.cdlwiki.model.Player;
 import com.jpreet.cdlwiki.repository.PlayerRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service(value = "playerService")
+@Transactional
 public class PlayerService {
 
     @Autowired
     private PlayerRepository playerRepo;
+
+    // need to define exception messages in separate file
+    public List<PlayerDTO> getAllPlayers() throws CDLWikiException {
+        List<Player> players = new ArrayList<>();
+        playerRepo.findAll().forEach(players::add);
+
+        if (players.isEmpty()) throw new CDLWikiException("Players not found");
+
+        List<PlayerDTO> playerDTOs = new ArrayList<>();
+        for (Player p : players) {
+            PlayerDTO pDTO = PlayerDTO.convertEntityToDTO(p);
+            playerDTOs.add(pDTO);
+        }
+
+        return playerDTOs;
+    }
+
+    public PlayerDTO getPlayerById(Integer id) throws CDLWikiException {
+        Optional<Player> optionalPlayer = playerRepo.findById(id);
+        Player player = optionalPlayer.orElseThrow(() -> new CDLWikiException("Player with id: " + id + " not found"));
+
+        return PlayerDTO.convertEntityToDTO(player);
+    }
 }
--- backend/src/main/java/com/jpreet/cdlwiki/service/TeamService.java
+++ backend/src/main/java/com/jpreet/cdlwiki/service/TeamService.java
@@ -1,11 +1,45 @@
 package com.jpreet.cdlwiki.service;
 
+import com.jpreet.cdlwiki.dto.PlayerDTO;
+import com.jpreet.cdlwiki.dto.TeamDTO;
+import com.jpreet.cdlwiki.exception.CDLWikiException;
+import com.jpreet.cdlwiki.model.Player;
+import com.jpreet.cdlwiki.model.Team;
 import com.jpreet.cdlwiki.repository.TeamRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service(value = "teamService")
+@Transactional
 public class TeamService {
     @Autowired
     private TeamRepository teamRepo;
+
+    // need to define exception messages in separate file
+    public List<TeamDTO> getAllTeams() throws CDLWikiException {
+        List<Team> teams = new ArrayList<>();
+        teamRepo.findAll().forEach(teams::add);
+
+        if (teams.isEmpty()) throw new CDLWikiException("Teams not found.");
+
+        List<TeamDTO> teamDTOs = new ArrayList<>();
+        for (Team t : teams) {
+            TeamDTO tDTO = TeamDTO.convertEntityToDTO(t);
+            teamDTOs.add(tDTO);
+        }
+
+        return teamDTOs;
+    }
+
+    public TeamDTO getTeamById(Integer id) throws CDLWikiException {
+        Optional<Team> optionalTeam = teamRepo.findById(id);
+        Team team = optionalTeam.orElseThrow(() -> new CDLWikiException("Team with id: " + id + " not found."));
+
+        return TeamDTO.convertEntityToDTO(team);
+    }
 }
