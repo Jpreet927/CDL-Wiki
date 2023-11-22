4cc6f41fc56e88eec665cee5cbc78ed55071c66e
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-11-21 19:46:05 -0500

    feat: :construction: working on api layer

--- backend/src/main/java/com/jpreet/cdlwiki/controller/PlayerController.java
+++ backend/src/main/java/com/jpreet/cdlwiki/controller/PlayerController.java
@@ -1,9 +1,31 @@
 package com.jpreet.cdlwiki.controller;
 
+import com.jpreet.cdlwiki.dto.PlayerDTO;
+import com.jpreet.cdlwiki.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
 public class PlayerController {
+    @Autowired
+    PlayerService playerService;
+
+    @GetMapping(value = "/players")
+    public List<PlayerDTO> getAllPlayers() {
+        List<PlayerDTO> players = new ArrayList<PlayerDTO>();
+        return players;
+    }
+
+    @GetMapping(value = "/players/{playerId}")
+    public PlayerDTO getPlayerById(@PathVariable Integer playerId) {
+        PlayerDTO playerDTO = new PlayerDTO();
+        return playerDTO;
+    }
 }
--- backend/src/main/java/com/jpreet/cdlwiki/controller/TeamController.java
+++ backend/src/main/java/com/jpreet/cdlwiki/controller/TeamController.java
@@ -1,9 +1,31 @@
 package com.jpreet.cdlwiki.controller;
 
+import com.jpreet.cdlwiki.dto.TeamDTO;
+import com.jpreet.cdlwiki.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
 public class TeamController {
+    @Autowired
+    PlayerService playerService;
+
+    @GetMapping(value = "/teams")
+    public List<TeamDTO> getAllTeams() {
+        List<TeamDTO> teams = new ArrayList<TeamDTO>();
+        return teams;
+    }
+
+    @GetMapping(value = "/teams/{teamId}")
+    public TeamDTO getTeamById(@PathVariable Integer teamId) {
+        TeamDTO team = new TeamDTO();
+        return team;
+    }
 }
