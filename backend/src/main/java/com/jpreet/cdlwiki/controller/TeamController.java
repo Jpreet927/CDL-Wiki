52c31ae69ce8ce6018e89d36cf3ac1e2c2283cfa
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-11-22 18:22:54 -0500

    feat: :safety_vest: added validated annotation to validate method params

--- backend/src/main/java/com/jpreet/cdlwiki/controller/TeamController.java
+++ backend/src/main/java/com/jpreet/cdlwiki/controller/TeamController.java
@@ -3,6 +3,7 @@ package com.jpreet.cdlwiki.controller;
 import com.jpreet.cdlwiki.dto.TeamDTO;
 import com.jpreet.cdlwiki.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Validated
 @RequestMapping(value = "/api")
 public class TeamController {
     @Autowired
