35da00df7da22e05828d9ee11659e377729b356c
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-01-15 18:14:37 -0500

    feat: :sparkles: implemented API endpoints for Round entity

--- backend/src/main/java/com/jpreet/cdlwiki/controller/RoundController.java
+++ backend/src/main/java/com/jpreet/cdlwiki/controller/RoundController.java
@@ -1,16 +1,15 @@
 package com.jpreet.cdlwiki.controller;
 
 import com.jpreet.cdlwiki.dto.RoundDTO;
+import com.jpreet.cdlwiki.dto.RoundRequest;
+import com.jpreet.cdlwiki.enums.RoundName;
 import com.jpreet.cdlwiki.exception.CDLWikiException;
 import com.jpreet.cdlwiki.service.RoundService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,5 +43,11 @@ public class RoundController {
     public ResponseEntity<List<RoundDTO>> getRoundByMajorId(@PathVariable Integer majorId) throws CDLWikiException {
         List<RoundDTO> rounds = roundService.getRoundByMajorId(majorId);
         return new ResponseEntity<>(rounds, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/")
+    public ResponseEntity<String> createRound(@RequestBody RoundRequest roundRequest) throws CDLWikiException {
+        String message = roundService.createRound(roundRequest);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/jpreet/cdlwiki/repository/RoundRepository.java
+++ backend/src/main/java/com/jpreet/cdlwiki/repository/RoundRepository.java
@@ -1,5 +1,6 @@
 package com.jpreet.cdlwiki.repository;
 
+import com.jpreet.cdlwiki.enums.RoundName;
 import com.jpreet.cdlwiki.model.Round;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -7,7 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface RoundRepository extends CrudRepository<Round, Integer> {
-    public List<Round> findByRound(String round);
-    @Query("SELECT r FROM Round r WHERE r.major.id = ?1")
+    public List<Round> findByRound(RoundName round);
+    @Query("SELECT r FROM Round r WHERE r.majorId = ?1")
     public List<Round> findByMajor(Integer majorId);
 }
