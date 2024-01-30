8f3331bbcefb797002a69da8c8ce71673caec50e
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-01-29 21:02:51 -0500

    chore: :heavy_plus_sign: configured path aliases for absolute imports

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,8 @@ export default {
             colors: {
                 "light-primary": "var(--light-primary)",
                 "light-secondary": "var(--light-secondary)",
+                "light-background": "var(--light-background)",
+                "light-background-2": "var(--light-background-2)",
                 "dark-primary": "var(--dark-primary)",
                 "dark-secondary": "var(--dark-secondary)",
             },
