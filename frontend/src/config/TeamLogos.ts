7ae8856a62ee555a5d46ca966b33db795b7106fa
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-01-29 21:04:47 -0500

    feat: :bento: added team logos / created constants

---
+++ frontend/src/config/TeamLogos.ts
@@ -0,0 +1,98 @@
+import fazeLight from "@/assets/logos/Atlanta_FaZe_Light.png";
+import fazeDark from "@/assets/logos/Atlanta_FaZe_Dark.png";
+import breachLight from "@/assets/logos/Boston_Breach_Light.png";
+import breachDark from "@/assets/logos/Boston_Breach_Dark.png";
+import ravens from "@/assets/logos/Carolina_Royal_Ravens.png";
+import thieves from "@/assets/logos/LA-Thieves.png";
+import legion from "@/assets/logos/Vegas_Legion.png";
+import lagLight from "@/assets/logos/LA_Guerillas_Light.png";
+import lagDark from "@/assets/logos/LA_Guerillas_Dark.png";
+import heretics from "@/assets/logos/Miami_Heretics.png";
+import rokkr from "@/assets/logos/Minnesota_ROKKR.png";
+import nysl from "@/assets/logos/NYSL.png";
+import opticLight from "@/assets/logos/OpTic_Texas_Light.png";
+import opticDark from "@/assets/logos/OpTic_Texas_Dark.png";
+import surge from "@/assets/logos/Seattle_Surge.png";
+import ultra from "@/assets/logos/Toronto_Ultra.png";
+
+type TeamLogo = {
+    team: string;
+    srcLight: string;
+    srcDark: string;
+    alt: string;
+};
+
+export const TEAM_LOGOS: TeamLogo[] = [
+    {
+        team: "Atlanta FaZe",
+        srcLight: fazeLight,
+        srcDark: fazeDark,
+        alt: "Atlanta FaZe logo",
+    },
+    {
+        team: "Boston Breach",
+        srcLight: breachLight,
+        srcDark: breachDark,
+        alt: "Boston Breach logo",
+    },
+    {
+        team: "Carolina Royal Ravens",
+        srcLight: ravens,
+        srcDark: ravens,
+        alt: "Carolina Royal Ravens logo",
+    },
+    {
+        team: "Las Vegas Legion",
+        srcLight: legion,
+        srcDark: legion,
+        alt: "Las Vegas Legion logo",
+    },
+    {
+        team: "LA Thieves",
+        srcLight: thieves,
+        srcDark: thieves,
+        alt: "LA Thieves logo",
+    },
+    {
+        team: "LA Guerillas",
+        srcLight: lagLight,
+        srcDark: lagDark,
+        alt: "LA Guerillas logo",
+    },
+    {
+        team: "Miami Heretics",
+        srcLight: heretics,
+        srcDark: heretics,
+        alt: "Miami Heretics logo",
+    },
+    {
+        team: "Minnesota RØKKR",
+        srcLight: rokkr,
+        srcDark: rokkr,
+        alt: "Minnesota RØKKR logo",
+    },
+    {
+        team: "New York Subliners",
+        srcLight: nysl,
+        srcDark: nysl,
+        alt: "New York Subliners logo",
+    },
+    {
+        team: "OpTic Texas",
+        srcLight: opticLight,
+        srcDark: opticDark,
+        alt: "OpTic Texas logo",
+    },
+    {
+        team: "Seattle Surge",
+        srcLight: surge,
+        srcDark: surge,
+        alt: "Seattle Surge logo",
+    },
+    {
+        team: "Toronto Ultra",
+        srcLight: ultra,
+        srcDark: ultra,
+        alt: "Toronto Ultra logo",
+    },
+];
