1375f0be496b5c34508936d024bb5b439ab02cbb
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-01-30 19:09:26 -0500

    feat: :sparkles: added team logos to hero

--- frontend/src/components/Home/Hero.tsx
+++ frontend/src/components/Home/Hero.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { heroImages, HeroImage } from "../../config/HeroImages";
+import { TEAM_LOGOS } from "../../config/TeamLogos";
 
 const Hero = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
@@ -7,29 +8,41 @@ const Hero = () => {
         heroImages[currentImageIndex]
     );
 
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setCurrentImageIndex((currentImageIndex + 1) % heroImages.length);
-    //         setCurrentImage(heroImages[currentImageIndex]);
-    //     }, 3000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((currentImageIndex + 1) % heroImages.length);
+            setCurrentImage(heroImages[currentImageIndex]);
+        }, 3000);
 
-    //     return () => clearInterval(interval);
-    // }, [currentImageIndex]);
+        return () => clearInterval(interval);
+    }, [currentImageIndex]);
 
     return (
         <div className="h-screen w-screen bg-cover relative">
             <div className="absolute bottom-0 left-0 bg-gradient-to-t from-black to-black/0 h-[50%] w-full"></div>
             <div className="absolute top-0 left-0 bg-gradient-to-b from-black to-black/0 h-[20%] w-full"></div>
-            <div className="px-48 py-48 absolute bottom-0 left-0">
+            <div className="px-48 py-36 absolute bottom-0 left-0 w-full flex flex-col gap-4">
                 <h1 className="text-8xl font-bold text-dark-primary">
                     Call of Duty League
                 </h1>
-                <p className="text-dark-secondary text-xl">2023/2024 Season</p>
+                <p className="text-dark-secondary text-xl mb-4">
+                    2023/2024 Season
+                </p>
+                <div className="flex justify-between">
+                    {TEAM_LOGOS.map((team) => (
+                        <img
+                            src={team.srcLight}
+                            alt={team.alt}
+                            className="h-[40px]"
+                        />
+                    ))}
+                </div>
             </div>
             <img
+                key={currentImageIndex}
                 src={currentImage.src}
                 alt={currentImage.alt}
-                className="w-full h-full object-cover transition-all duration-500"
+                className="w-full h-full object-cover select-none"
             />
         </div>
     );
