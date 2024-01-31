c0a118c9f4b594be74127fb7b937a8df7c5a8ab2
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-01-30 19:10:37 -0500

    fix: :recycle: extracted nav items to config file and fixed navbar positioning and background when scrolling

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import cdlWhite from "../assets/logos/CDL-logo-white.png";
 
 const Navbar = () => {
+    const [backgroundVisible, setBackgroundVisible] = useState(false);
+
+    useEffect(() => {
+        // adds background to navbar when user scrolls
+        const toggleBackground = () => {
+            if (window.scrollY > 100) {
+                setBackgroundVisible(true);
+            } else {
+                setBackgroundVisible(false);
+            }
+        };
+
+        window.addEventListener("scroll", toggleBackground);
+    }, []);
+
     return (
-        <nav className="absolute top-0 left-0 w-full flex justify-between items-center py-8 px-24 z-[998] text-dark-secondary">
+        <nav
+            className={`fixed top-0 left-0 w-full flex justify-between items-center py-8 px-24 z-[998] text-dark-secondary transition-all ease-in-out duration-300 ${
+                backgroundVisible ? "bg-black/80 backdrop-blur-xl" : ""
+            }`}
+        >
             <img src={cdlWhite} alt="CDL Logo" className="h-[30px]" />
             <ul className="flex items-center gap-24">
                 <li className="hover:text-dark-primary transition-colors ease-in-out duration-300">
---
+++ frontend/src/config/NavItems.ts
@@ -0,0 +1,77 @@
+export const NAV_ITEMS = [
+    {
+        title: "Home",
+        href: "/",
+    },
+    {
+        title: "Standings",
+        href: "/standings",
+    },
+    {
+        title: "Teams",
+        href: "/teams",
+    },
+    {
+        title: "Players",
+        href: "/players",
+    },
+    {
+        title: "Matches",
+        href: "/matches",
+    },
+    {
+        title: "About",
+        href: "/about",
+    },
+];
+
+export const TEAMS_NAV_ITEMS = [
+    {
+        team: "Atlanta FaZe",
+        href: "/teams/1",
+    },
+    {
+        team: "Boston Breach",
+        href: "/teams/2",
+    },
+    {
+        team: "Carolina Ravens",
+        href: "/teams/3",
+    },
+    {
+        team: "Las Vegas Legion",
+        href: "/teams/4",
+    },
+    {
+        team: "LA Guerillas",
+        href: "/teams/5",
+    },
+    {
+        team: "LA Thieves",
+        href: "/teams/6",
+    },
+    {
+        team: "Miami Heretics",
+        href: "/teams/7",
+    },
+    {
+        team: "Minnesota RØKKR",
+        href: "/teams/8",
+    },
+    {
+        team: "NYSL",
+        href: "/teams/9",
+    },
+    {
+        team: "OpTic Texas",
+        href: "/teams/10",
+    },
+    {
+        team: "Seattle Surge",
+        href: "/teams/11",
+    },
+    {
+        team: "Toronto Ultra",
+        href: "/teams/12",
+    },
+];
