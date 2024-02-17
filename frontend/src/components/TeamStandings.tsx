9a7e375a8264fe074cdf3b39b7b2dabe5ba6fd94
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-02-16 19:41:32 -0500

    feat: :sparkles: created standings table/page

---
+++ frontend/src/components/TeamStandings.tsx
@@ -0,0 +1,31 @@
+import { Team } from "@/ts/types/Team";
+import { Link } from "react-router-dom";
+
+const TeamStandings = ({ team, idx }: { team: Team; idx: number }) => {
+    return (
+        <tr className="grid sm:grid-cols-7 grid-cols-5 md:px-12 px-2 py-6 gap-2 w-full bg-background hover:bg-background-2 transition-colors ease-in-out duration-100 border-b-background-2 border-b items-center">
+            <p>{idx + 1}</p>
+            <Link
+                to={`/teams/${team.id}`}
+                className="flex gap-2 col-start-2 sm:col-end-5 col-end-4"
+            >
+                <div className="w-[25%] flex justify-center items-center">
+                    <img
+                        src={team.logoLight}
+                        alt={team.name + "logo"}
+                        className="max-h-[30px]"
+                    />
+                </div>
+                <p className="sm:block hidden">{team.name}</p>
+            </Link>
+            <div className="sm:hidden block">
+                <p>10/5</p>
+            </div>
+            <p className="sm:block hidden">10</p>
+            <p className="sm:block hidden">5</p>
+            <p>{team.points}</p>
+        </tr>
+    );
+};
+
+export default TeamStandings;
--- frontend/src/pages/StandingsPage.tsx
+++ frontend/src/pages/StandingsPage.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import TeamStandings from "@/components/TeamStandings";
+import Section from "@/components/templates/Section";
+import { TEAM_DATA } from "@/ts/constants/TeamData";
 
 const StandingsPage = () => {
-    return <div>StandingsPage</div>;
+    return (
+        <div className="py-12">
+            <Section title="League Standings">
+                <table>
+                    <th className="grid sm:grid-cols-7 grid-cols-5 md:px-12 px-2 pt-8 pb-2 gap-2 w-full items-center [&>*]:font-bold text-left">
+                        <p>Rank</p>
+                        <div className="col-start-2 sm:col-end-5 col-end-4 md:pl-12">
+                            <p>Team</p>
+                        </div>
+                        <div className="sm:hidden block">
+                            <p>W/L</p>
+                        </div>
+                        <p className="sm:block hidden">Wins</p>
+                        <p className="sm:block hidden">Losses</p>
+                        <p>Points</p>
+                    </th>
+                    <tbody>
+                        {TEAM_DATA.map((team, idx) => (
+                            <TeamStandings team={team} idx={idx} />
+                        ))}
+                    </tbody>
+                </table>
+            </Section>
+        </div>
+    );
 };
 
 export default StandingsPage;
