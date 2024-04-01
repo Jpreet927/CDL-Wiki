d32f2fe08efa1b81015576572806770fd4c1376b
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-03-31 21:18:54 -0400

    chore: :bento: updated hero images

--- frontend/src/config/HeroImages.ts
+++ frontend/src/config/HeroImages.ts
@@ -1,6 +1,7 @@
 import hero1 from "../assets/images/CDL-Wiki-Hero.jpg";
 import hero2 from "../assets/images/MW2_CDL2023_Major_I_Winner.jpg";
 import hero3 from "../assets/images/CDL-Trophy.jpg";
+import hero4 from "@/assets/images/FaZe_Major_I.jpg";
 
 export type HeroImage = {
     src: string;
@@ -14,10 +15,14 @@ export const heroImages: HeroImage[] = [
     },
     {
         src: hero2,
-        alt: "The New York Subliners celebrating their win at Major 1 2023",
+        alt: "The New York Subliners celebrating their win at CDL Champs 2023",
     },
     {
         src: hero3,
         alt: "The CDL Championship Trophy in green lighting with team logos in the background",
+    },
+    {
+        src: hero4,
+        alt: "Atlanta FaZe celebrating their victory at Major 2 2024",
     },
 ];
