8f3504e052487a299eb39185dee9b87b9bcb8ea8
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-05-01 18:34:48 -0400

    fix: :iphone: fixed responsiveness issues on the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,11 +5,15 @@ import cdlWhite from "../assets/logos/CDL-logo-white.png";
 import cdlDark from "../assets/logos/CDL-logo-black.png";
 import { ThemeContext } from "@/context/ThemeProvider";
 import Logo from "./templates/Logo";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Navbar = () => {
     const location = useLocation();
     const [backgroundVisible, setBackgroundVisible] = useState(false);
+    const [mobileNavOpen, setMobileNavOpen] = useState(false);
     const { theme } = useContext(ThemeContext);
+    const background = theme === "dark" ? "bg-black/80" : "bg-white/80";
 
     useEffect(() => {
         // adds background to navbar when user scrolls
@@ -21,65 +25,99 @@ const Navbar = () => {
             }
         };
 
+        // reset mobile nav state if window size changes while mobile nav is open
+        const removeNavBackground = () => {
+            if (window.innerWidth > 768) {
+                setMobileNavOpen(false);
+            }
+        };
+
         window.addEventListener("scroll", toggleBackground);
+        window.addEventListener("resize", removeNavBackground);
     }, []);
 
     return (
         <nav
-            className={`fixed top-0 left-0 w-full flex justify-between items-center py-8 px-24 z-[998] text-primary transition-all ease-in-out duration-300 ${
-                backgroundVisible
-                    ? `${
-                          theme === "dark" ? "bg-black/80" : "bg-white/80"
-                      } backdrop-blur-xl`
+            className={`fixed top-0 left-0 w-full flex md:flex-row flex-col justify-between items-center py-8 xl:px-24 lg:px-12 px-8 z-[998] text-primary transition-all ease-in-out duration-300 ${
+                backgroundVisible || mobileNavOpen
+                    ? `${background} backdrop-blur-xl`
                     : ""
             }`}
         >
-            <div className="h-[30px]">
-                <Logo srcDark={cdlWhite} srcLight={cdlDark} alt="CDL Logo" />
+            <div className="flex w-full justify-between items-center">
+                <div className="h-[30px]">
+                    <Logo
+                        srcDark={cdlWhite}
+                        srcLight={cdlDark}
+                        alt="CDL Logo"
+                    />
+                </div>
+                <div className="md:hidden block">
+                    {mobileNavOpen ? (
+                        <CloseIcon
+                            className="cursor-pointer"
+                            onClick={() => setMobileNavOpen(false)}
+                        />
+                    ) : (
+                        <MenuIcon
+                            className="cursor-pointer"
+                            onClick={() => setMobileNavOpen(true)}
+                        />
+                    )}
+                </div>
             </div>
-            <ul className="flex items-center gap-24">
+            <ul
+                className={`md:flex md:items-center md:w-auto w-full md:h-auto h-screen md:py-0 py-12 md:px-0 px-4 md:flex-row flex-col ${
+                    mobileNavOpen ? "flex" : "hidden"
+                } lg:gap-24 gap-12`}
+            >
                 <li
-                    className={`hover:opacity-100 transition-opacity ease-in-out duration-300 ${
+                    className={`hover:opacity-100 transition-opacity ease-in-out duration-300 md:text-base text-2xl ${
                         location.pathname === "/"
                             ? "opacity-100 underline underline-offset-8"
                             : "opacity-75"
                     }`}
+                    onClick={() => setMobileNavOpen(false)}
                 >
                     <Link to="/">Home</Link>
                 </li>
                 <li
-                    className={`hover:opacity-100 transition-opacity ease-in-out duration-300 ${
+                    className={`hover:opacity-100 transition-opacity ease-in-out duration-300 md:text-base text-2xl ${
                         location.pathname.includes("/teams")
                             ? "opacity-100 underline underline-offset-8"
                             : "opacity-75"
                     }`}
+                    onClick={() => setMobileNavOpen(false)}
                 >
                     <Link to="/teams">Teams</Link>
                 </li>
                 <li
-                    className={`hover:opacity-100 transition-opacity ease-in-out duration-300 ${
+                    className={`hover:opacity-100 transition-opacity ease-in-out duration-300 md:text-base text-2xl ${
                         location.pathname.includes("/standings")
                             ? "opacity-100 underline underline-offset-8"
                             : "opacity-75"
                     }`}
+                    onClick={() => setMobileNavOpen(false)}
                 >
                     <Link to="/standings">Standings</Link>
                 </li>
                 <li
-                    className={`hover:opacity-100 transition-opacity ease-in-out duration-300 ${
+                    className={`hover:opacity-100 transition-opacity ease-in-out duration-300 md:text-base text-2xl ${
                         location.pathname.includes("/matches")
                             ? "opacity-100 underline underline-offset-8"
                             : "opacity-75"
                     }`}
+                    onClick={() => setMobileNavOpen(false)}
                 >
                     <Link to="/matches">Matches</Link>
                 </li>
                 <li
-                    className={`hover:opacity-100 transition-opacity ease-in-out duration-300 ${
+                    className={`hover:opacity-100 transition-opacity ease-in-out duration-300 md:text-base text-2xl ${
                         location.pathname.includes("/about")
                             ? "opacity-100 underline underline-offset-8"
                             : "opacity-75"
                     }`}
+                    onClick={() => setMobileNavOpen(false)}
                 >
                     <Link to="/about">About</Link>
                 </li>
