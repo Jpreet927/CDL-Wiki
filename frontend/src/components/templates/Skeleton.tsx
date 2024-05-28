5ebbb223e163c6675e6f8d7dfee9b0d52c745c34
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-05-28 19:17:26 -0400

    feat: :sparkles: added an optional delay prop to Skeleton to offset animation

--- frontend/src/components/templates/Skeleton.tsx
+++ frontend/src/components/templates/Skeleton.tsx
@@ -1,6 +1,14 @@
-const Skeleton = () => {
+const Skeleton = ({ delay = 0 }: { delay?: number }) => {
+    const animationStyle = {
+        animationDelay: `${delay}ms`,
+        animationFillMode: "both",
+    };
+
     return (
-        <div className="min-w-full min-h-full animate-pulse bg-background-2"></div>
+        <div
+            className="min-w-full min-h-full animate-pulse bg-background-2 delay-75"
+            style={animationStyle}
+        ></div>
     );
 };
 
