f9443e9aec3a1a99d9131c6a4db980c8d4979537
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-05-30 18:58:55 -0400

    refactor: :recycle: refactored useEffect to react query

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -66,7 +66,7 @@ const HomePage = () => {
                         ))}
 
                     {upcomingMatchesPending &&
-                        Array(3)
+                        Array(9)
                             .fill(null)
                             .map((_, idx) => {
                                 return (
--- frontend/src/pages/TeamsPage.tsx
+++ frontend/src/pages/TeamsPage.tsx
@@ -1,29 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
 import { getTeams } from "@/api/Teams";
 import TeamCard from "@/components/TeamCard";
 import Page from "@/components/templates/Page";
 import Section from "@/components/templates/Section";
+import Skeleton from "@/components/templates/Skeleton";
 import { Team } from "@/ts/types/Team";
-import { useEffect, useState } from "react";
 
 const TeamsPage = () => {
-    const [teams, setTeams] = useState<Team[] | null>(null);
-
-    useEffect(() => {
-        getTeams().then((teams) => setTeams(teams));
-    }, []);
+    const {
+        isPending,
+        error,
+        data: teams,
+    } = useQuery({ queryKey: ["teamData"], queryFn: getTeams });
 
     return (
         <Page title={"Call of Duty League Teams"} bannerType="DEFAULT">
             <Section title="Teams">
                 <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
                     {teams &&
-                        teams.map((team) => (
+                        teams.map((team: Team) => (
                             <TeamCard
                                 team={team}
                                 variant="VIDEO"
                                 key={team.id}
                             />
                         ))}
+
+                    {isPending &&
+                        Array(9)
+                            .fill(null)
+                            .map((_, idx) => {
+                                return (
+                                    <div className="aspect-video">
+                                        <Skeleton delay={idx * 100} />
+                                    </div>
+                                );
+                            })}
+                    {error && (
+                        <p className="text-secondary text-center mt-4">
+                            {error.message}
+                        </p>
+                    )}
                 </div>
             </Section>
         </Page>
